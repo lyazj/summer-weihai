8bd724702d1cdc6d4aa00694003ccb54fddb310b
stu <gengjingbo21@mails.ucas.ac.cn>
2024-08-07 17:53:45 +0800

    Finish analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -17,5 +17,15 @@ def decode_edep(events, i):
 INPUT = 'tree.root'
 events = uproot.open(INPUT + ':tree').arrays()
 print('Found fields:', events.fields)
+[y, x] = np.meshgrid(range(50), range(50))
 for i in range(len(events)):
     Edep = decode_edep(events, i)
+    xy0 = np.argmax(Edep)
+    x0, y0 = xy0 // 50, xy0 % 50
+    showernum = 3.5 * 50 / 50.5
+    distance = np.hypot(x - x0, y - y0)
+    weight = distance <= showernum
+    sumE = np.sum(Edep * weight)
+    meanX = np.sum(x * Edep * weight) / sumE
+    meanY = np.sum(y * Edep * weight) / sumE
+    print('%4d: %.3e\t%.3e\t%.3e\t%.3e' %(i, x0, y0, meanX, meanY))
