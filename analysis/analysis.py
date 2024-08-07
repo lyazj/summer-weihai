7ed3509f46be956190db91cc08196095c970e8c6
lyazj <seeson@pku.edu.cn>
2024-08-07 17:19:31 +0800

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,4 +1,5 @@
 import uproot
+import numpy as np
 
 def decode_position(events):
     Layer = events['Pos'] // (events['NCellX'] * events['NCellY'])
@@ -6,11 +7,15 @@ def decode_position(events):
     YCell = events['Pos'] % events['NCellY']
     return Layer, XCell, YCell
 
+def decode_edep(events, i):
+    Layer, XCell, YCell = decode_position(events[i])
+    Edep = np.zeros((events[i]['NLayer'], events[i]['NCellX'], events[i]['NCellY']))
+    for l, x, y, e in zip(Layer, XCell, YCell, events[i]['Edep']):
+        Edep[l, x, y] = e
+    return Edep.sum(axis=0)
+
 INPUT = 'tree.root'
 events = uproot.open(INPUT + ':tree').arrays()
 print('Found fields:', events.fields)
-Layer, XCell, YCell = decode_position(events)
-print('Layer:', Layer)
-print('XCell:', XCell)
-print('YCell:', YCell)
-Edep = events['Edep']
+for i in range(len(events)):
+    Edep = decode_edep(events, i)
